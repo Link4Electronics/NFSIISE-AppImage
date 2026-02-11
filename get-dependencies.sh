15fc148f7aa409910e8d558393f4b3807d7289d4
Link4Electronics <link4electronics@gmail.com>
2026-02-11 16:03:23 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,14 +7,16 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    lib32-sdl2 \
     libdecor
+    yasm
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package nfs2se-git
 
 # If the application needs to be manually built that has to be done down here
 
